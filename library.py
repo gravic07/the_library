cf7e48e51b22ae1b7729aba8dd4c51c08adfee73
Grant Vickers <grant.vickers@gmail.com>
2015-07-22 03:03:46 +0000

    CRUD functionality added

--- library.py
+++ library.py
@@ -143,6 +143,6 @@ app.secret_key = '''
     '''
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run(host = '0.0.0.0', port = 5000)
