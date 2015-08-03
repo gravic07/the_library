2dc373ac1263ed25aa86d42a29c2f80f3f28576d
Grant Vickers <grant.vickers@gmail.com>
2015-08-03 06:00:51 +0000

    Debugging login

--- library.py
+++ library.py
@@ -247,7 +247,7 @@ def fbconnect():
 
     print "Check #2"
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
-    h = httplib2.Http()
+    h = httplib2.Http(disable_ssl_certificate_validation=True)
     result = h.request(url, 'GET')[1]
     print "Result:", result
 
