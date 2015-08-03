9a2082f68357d4d387405809c46b580cddea3245
Grant Vickers <grant.vickers@gmail.com>
2015-08-03 04:54:01 +0000

    Debugging login

--- library.py
+++ library.py
@@ -240,7 +240,9 @@ def fbconnect():
     # app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
 
     app_id = os.environ['FACEBOOK_APP_ID']
+    print "App ID:", app_id
     app_secret = os.environ['FACEBOOK_SECRET']
+    print "App Secret:", app_secret
 
     print "Check #2"
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
