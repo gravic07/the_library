e436b1a53396a8da42951790b4438ed4d1ffffd0
Grant Vickers <grant.vickers@gmail.com>
2015-08-03 05:15:15 +0000

    Debugging login

--- library.py
+++ library.py
@@ -248,6 +248,7 @@ def fbconnect():
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
+    print "Result:", result
 
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.2/me"
