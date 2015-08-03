2d50dd96cdd26e0e2d5bc0f094ab84bef19317be
Grant Vickers <grant.vickers@gmail.com>
2015-08-03 19:37:40 +0000

    Debuggin SSL cert error

--- library.py
+++ library.py
@@ -224,7 +224,8 @@ def fbconnect():
     app_secret = fb_client_secrets['web']['app_secret']
 
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
-    h = httplib2.Http(disable_ssl_certificate_validation=True)
+    # h = httplib2.Http(disable_ssl_certificate_validation=True)
+    h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
     # Strip expired tag from access token
