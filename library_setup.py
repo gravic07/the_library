d1a7b4db624984da7150fe5de7aa0ef88f9fdef7
Grant Vickers <grant.vickers@gmail.com>
2015-08-05 06:13:23 +0000

    Adjusted print statement

--- library_setup.py
+++ library_setup.py
@@ -69,4 +69,3 @@ engine = create_engine('sqlite:///theArchive.db')
 # engine = create_engine('postgres://ewcuvsjxbhzuce:lTxnaKjAsx3L5JVCsjN1NXrrnS@ec2-54-83-20-177.compute-1.amazonaws.com:5432/d6l2vgh7udooqv')
 # Create all tables in the declarative base.
 Base.metadata.create_all(engine)
-print "Database setup"
